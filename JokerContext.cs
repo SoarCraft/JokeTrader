e772fba9f26457834ea95e5a26b04dcf920cce3b
Aloento <11802769+Aloento@users.noreply.github.com>
2024-12-23 23:41:59 +0100

    [feat] 添加 JokerContext 和模型类以支持数据库操作

---
+++ JokerContext.cs
@@ -0,0 +1,9 @@
+﻿namespace JokeTrader;
+
+using Microsoft.EntityFrameworkCore;
+
+public class JokerContext(DbContextOptions<JokerContext> options) : DbContext(options) {
+    public DbSet<Symbol> Symbols { get; set; }
+
+    public DbSet<BTCKLine> BTCKLines { get; set; }
+}
---
+++ Models.cs
@@ -0,0 +1,27 @@
+﻿namespace JokeTrader;
+
+using System.ComponentModel.DataAnnotations;
+
+public abstract class BasicKLine {
+    [Key]
+    public DateTime StartTime { get; set; }
+
+    public double OpenPrice { get; set; }
+
+    public Symbol Symbol { get; set; }
+}
+
+public class BTCKLine : BasicKLine;
+
+public class Symbol {
+    [Key]
+    public string Name { get; set; }
+
+    public double MaxOrderValue { get; set; }
+
+    public double MinOrderValue { get; set; }
+
+    public DateTime LastUpdated { get; set; }
+
+    public List<BTCKLine> BTCKLines { get; set; } = [];
+}
