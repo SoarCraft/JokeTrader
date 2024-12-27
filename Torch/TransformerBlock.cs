e8967ad0f591994c36b69a041ed154e13d897076
Aloento <11802769+Aloento@users.noreply.github.com>
2024-12-27 22:46:13 +0100

    [feat] 添加 TransformerBlock 模块以实现变换器结构

---
+++ Torch/TransformerBlock.cs
@@ -0,0 +1,43 @@
+﻿namespace JokeTrader.Torch;
+
+using TorchSharp.Modules;
+using static TorchSharp.torch;
+using static TorchSharp.torch.nn;
+
+internal class TransformerBlock : Module<Tensor, Tensor> {
+    private readonly FlashMultiHeadAttention attention;
+
+    private readonly Sequential feedForward;
+
+    private readonly LayerNorm norm1;
+
+    private readonly LayerNorm norm2;
+
+    private readonly Dropout dropout;
+
+    public TransformerBlock(int embedDim, int numHeads, float dropoutRate) : base(nameof(TransformerBlock)) {
+        this.attention = new(embedDim, numHeads, dropoutRate);
+
+        this.feedForward = Sequential(
+            Linear(embedDim, embedDim * 4),
+            ReLU(),
+            Linear(embedDim * 4, embedDim)
+        );
+
+        this.norm1 = LayerNorm(embedDim);
+        this.norm2 = LayerNorm(embedDim);
+        this.dropout = Dropout(dropoutRate);
+
+        this.RegisterComponents();
+    }
+
+    public override Tensor forward(Tensor input) {
+        using var _ = NewDisposeScope();
+
+        var attnOutput = this.attention.forward(this.norm1.forward(input));
+        input += this.dropout.forward(attnOutput);
+
+        var ffnOutput = this.feedForward.forward(this.norm2.forward(input));
+        return (input + this.dropout.forward(ffnOutput)).MoveToOuterDisposeScope();
+    }
+}
