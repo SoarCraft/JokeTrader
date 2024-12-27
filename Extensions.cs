2db837139cd5bde1b706eb7fc77fdabee412eb08
Aloento <11802769+Aloento@users.noreply.github.com>
2024-12-27 17:36:21 +0100

    [feat] 添加扩展方法以计算均值和标准差

---
+++ Extensions.cs
@@ -0,0 +1,23 @@
+﻿namespace JokeTrader;
+
+public static class Extensions {
+    public static double CalculateMean<T>(this IEnumerable<T> source, Func<T, double> selector) {
+        var array = source as T[] ?? source.ToArray();
+
+        var sum = array.Select(selector).Sum();
+        var count = array.Length;
+
+        return count > 0 ? sum / count : 0;
+    }
+
+    public static double CalculateStd<T>(this IEnumerable<T> source, Func<T, double> selector, double mean) {
+        var array = source as T[] ?? source.ToArray();
+
+        var count = array.Length;
+        if (count <= 1)
+            return 0;
+
+        var sum = array.Select(data => Math.Pow(selector(data) - mean, 2)).Sum();
+        return Math.Sqrt(sum / (count - 1));
+    }
+}
