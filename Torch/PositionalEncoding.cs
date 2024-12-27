d7ca4bb9300b9aefe8f71ce39c37d47e923c78ac
Aloento <11802769+Aloento@users.noreply.github.com>
2024-12-27 22:46:06 +0100

    [feat] 添加 PositionalEncoding 模块以实现位置编码功能

---
+++ Torch/PositionalEncoding.cs
@@ -0,0 +1,26 @@
+﻿namespace JokeTrader.Torch;
+
+using static TorchSharp.torch;
+using static TorchSharp.torch.nn;
+
+internal class PositionalEncoding() : Module<Tensor, Tensor>(nameof(PositionalEncoding)) {
+    public override Tensor forward(Tensor input) {
+        using var _ = NewDisposeScope();
+
+        var batchSize = input.shape[0];
+        var seqLen = input.shape[1];
+        var embedDim = input.shape[2];
+
+        var intervals = input[.., .., -1];
+        var features = input[.., .., ..^1];
+
+        var posEncoding = zeros(batchSize, seqLen, embedDim, device: input.device);
+        for (var i = 0; i < (embedDim - 1) / 2; i++) {
+            var div = exp(-log(10000.0) * (2 * i) / embedDim);
+            posEncoding[.., .., 2 * i] = sin(intervals.unsqueeze(-1) * div);
+            posEncoding[.., .., 2 * i + 1] = cos(intervals.unsqueeze(-1) * div);
+        }
+
+        return (features + posEncoding).MoveToOuterDisposeScope();
+    }
+}
